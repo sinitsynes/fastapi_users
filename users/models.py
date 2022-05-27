bc7dfed1862076a80b626ca9708fdb678668d6a9
Evgeny Sinitsyn <eugean.sinitsyn@gmail.com>
2022-05-27 22:13:27 +0300

    filter_queries

--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    department_id = Column(Integer, ForeignKey('departments.id'))
     username = Column(String(200))
     password = Column(String(128))
     email = Column(String(120))
@@ -31,3 +32,18 @@ class UserRoles(Base):
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     role_id = Column(Integer, ForeignKey('role.id'), primary_key=True)
     date_created = Column(Date, default=date.today())
+
+
+class Department_Level(Base):
+    __tablename__ = 'dep_level'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+
+class Departments(Base):
+    __tablename__ = 'departments'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    description = Column(String(20))
+    department_level = Column(Integer, ForeignKey('dep_level.id'))
+
--- users/schemas.py
+++ users/schemas.py
@@ -15,6 +15,7 @@ class User_In_DB(BaseModel):
     username: str
     password: str
     email: str
+    department_id: int
     roles: list[int]
 
     class Config:
@@ -22,6 +23,17 @@ class User_In_DB(BaseModel):
 
 class UserResponse(BaseModel):
     username: str
+    roles: list[int]
+    department_id: int
+    department_level: int
+
+    class Config:
+        orm_mode = True
+
+
+class Department(BaseModel):
+    description: str
+    department_level: int
 
     class Config:
         orm_mode = True
